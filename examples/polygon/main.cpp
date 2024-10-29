2f2f2792f955149a8c312e18c1be8a635aa7ed50
Fabien Evrard <fa.evrard@gmail.com>
2024-10-29 14:47:53 -0500

    New small example that intersects plane with cuboid and computes surface area

---
+++ examples/polygon/main.cpp
@@ -0,0 +1,36 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#include "irl/generic_cutting/cut_polygon.h"
+
+int main(int argc, char* argv[]) {
+  // Defining bounds of cuboid cell
+  const auto x0 = IRL::Pt(0.0, 0.0, 0.0);
+  const auto x1 = IRL::Pt(1.0, 1.0, 1.0);
+
+  // Construct cell object
+  const auto cell = IRL::RectangularCuboid::fromBoundingPts(x0, x1);
+
+  for (int i = 0; i < 110; i++) {
+    // Define plane normal and distance
+    auto normal = IRL::Normal(1.0, 2.0, 3.0);
+    normal.normalize();
+    const auto distance = static_cast<double>(i) * 0.01;
+
+    // Create plane object
+    const auto plane = IRL::Plane(normal, distance);
+
+    // Compute polygon
+    const auto polygon = IRL::cutPlaneByHexahedron<IRL::Polygon>(cell, plane);
+
+    // Print surface area
+    std::cout << std::scientific
+              << "Surface area of polygon = " << std::setprecision(3)
+              << polygon.calculateVolume()
+              << " and distance = " << std::setprecision(3) << distance
+              << std::endl;
+  }
+
+  return 0;
+}
