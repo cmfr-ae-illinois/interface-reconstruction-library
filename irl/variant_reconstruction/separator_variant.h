d3af252eb8a0194402a4153eee6b9465202418de
Fabien Evrard <fa.evrard@gmail.com>
2025-06-03 15:08:17 -0500

    Creating new separator variant class which is a type-safe union of PlanarSeparator, Paraboloid, and Cylinder (*this could be completed with new types when needed).

---
+++ irl/variant_reconstruction/separator_variant.h
@@ -0,0 +1,22 @@
+// This file is part of the Interface Reconstruction Library (IRL),
+// a library for interface reconstruction and computational geometry operations.
+//
+// Copyright (C) 2025 Fabien Evrard <fa.evrard@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef IRL_PLANAR_RECONSTRUCTION_SEPARATOR_VARIANT_H_
+#define IRL_PLANAR_RECONSTRUCTION_SEPARATOR_VARIANT_H_
+
+#include <variant>
+#include "irl/cylinder_reconstruction/cylinder.h"
+#include "irl/paraboloid_reconstruction/paraboloid.h"
+#include "irl/planar_reconstruction/planar_separator.h"
+
+namespace IRL {
+using SeparatorVariant = std::variant<PlanarSeparator, Paraboloid, Cylinder>;
+}  // namespace IRL
+
+#endif  // IRL_PLANAR_RECONSTRUCTION_SEPARATOR_VARIANT_H_
