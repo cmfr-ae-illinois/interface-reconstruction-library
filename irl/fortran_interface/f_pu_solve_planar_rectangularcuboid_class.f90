81a42c9d14e3c595ee29dec9f8d81d1576adc9aa
Ilia Kheirkhah <iliak2@illinois.edu>
2025-07-31 13:37:19 -0500

    PU_Solve Fortran Interface Update

--- irl/fortran_interface/f_pu_solve_planar_rectangularcuboid_class.f90
+++ irl/fortran_interface/f_pu_solve_planar_rectangularcuboid_class.f90
@@ -2,78 +2,82 @@
 
 !! Interface for PUSolve Class's solve function
 
-module f_PUSolve_Planar_RectCub_class
+module f_PUSolve_RectCub_class
     use f_RectCub_class
-    use f_PlanarSep_class
+    use f_SeparatorVariant_class
     use, intrinsic :: iso_c_binding
     use f_DefinedTypes
     implicit none
 
-    type, public, bind(C) :: c_PUST_Planar_RectCub
+    type, public, bind(C) :: c_PUST_RectCub
         type(C_PTR), private :: object = C_NULL_PTR
-    end type c_PUST_Planar_RectCub
+    end type c_PUST_RectCub
 
-    type, public :: PUST_Planar_RectCub_type
-        type(c_PUST_Planar_RectCub) :: c_object
+    type, public :: PUST_RectCub_type
+        type(c_PUST_RectCub) :: c_object
     contains
-        final :: PUST_Planar_RectCub_class_delete
-    end type PUST_Planar_RectCub_type
+        final :: PUST_RectCub_class_delete
+    end type PUST_RectCub_type
 
     interface new
-        module procedure PUST_Planar_RectCub_class_new
+        module procedure PUST_RectCub_class_new
     end interface
-    interface solve
-        module procedure PUST_Planar_RectCub_class_solve
+    interface solveEdge
+        module procedure PUST_RectCub_class_solveEdge
     end interface
 
     interface
 
-        subroutine F_PUST_Planar_RectCub_new(this) &
-            bind(C, name = "c_PUST_Planar_RectCub_new")
+        subroutine F_PUST_RectCub_new(this) &
+            bind(C, name = "c_PUST_RectCub_new")
             import
             implicit none
-            type(c_PUST_Planar_RectCub) :: this
-        end subroutine F_PUST_Planar_RectCub_new
+            type(c_PUST_RectCub) :: this
+        end subroutine F_PUST_RectCub_new
 
-        subroutine F_PUST_Planar_RectCub_delete(this) & 
-            bind(C, name = "c_PUST_Planar_RectCub_delete")
+        subroutine F_PUST_RectCub_delete(this) & 
+            bind(C, name = "c_PUST_RectCub_delete")
             import
             implicit none
-            type(c_PUST_Planar_RectCub) :: this
-        end subroutine F_PUST_Planar_RectCub_delete
+            type(c_PUST_RectCub) :: this
+        end subroutine F_PUST_RectCub_delete
 
-        subroutine F_PUST_Planar_RectCub_solve(this,surface_tension_coefficient, a_force)
-            bind(C, name = "c_PUST_Planar_RectCub_solve")
+        subroutine F_PUST_RectCub_solveEdge(this,surface_tension_coefficient, start_point, end_point, a_force)
+            bind(C, name = "c_PUST_RectCub_solveEdge")
             import
             implicit none
-            type(c_PUST_Planar_RectCub) :: this
+            type(c_PUST_RectCub) :: this
             real(C_DOUBLE) :: surface_tension_coefficient
+            real(C_DOUBLE), dimension(*), intent(in) :: start_point ! dimension(1:3)
+            real(C_DOUBLE), dimension(*), intent(in) :: end_point ! dimension(1:3)
             real(C_DOUBLE), dimension(*), intent(out) :: a_force
-        end subroutine F_PUST_Planar_RectCub_solve
+        end subroutine F_PUST_RectCub_solveEdge
     
     end interface
 
     contains
 
-        subroutine PUST_Planar_RectCub_class_new(this)
+        subroutine PUST_RectCub_class_new(this)
             implicit none
-            type(PUST_Planar_RectCub_type), intent(inout) :: this
-            call F_PUST_Planar_RectCub_new(this%c_object)
-        end subroutine PUST_Planar_RectCub_class_new
+            type(PUST_RectCub_type), intent(inout) :: this
+            call F_PUST_RectCub_new(this%c_object)
+        end subroutine PUST_RectCub_class_new
 
-        impure elemental subroutine PUST_Planar_RectCub_class_delete(this)
+        impure elemental subroutine PUST_RectCub_class_delete(this)
             implicit none
-            type(PUST_Planar_RectCub_type), intent(in) :: this
-            call F_PUST_Planar_RectCub_delete(this%c_object)
-        end subroutine PUST_Planar_RectCub_class_delete
+            type(PUST_RectCub_type), intent(in) :: this
+            call F_PUST_RectCub_delete(this%c_object)
+        end subroutine PUST_RectCub_class_delete
 
-        subroutine PUST_Planar_RectCub_class_solve(this,surface_tension_coefficient, a_force)
+        subroutine PUST_RectCub_class_solveEdge(this,surface_tension_coefficient, start_point, end_point, a_force)
             implicit none
-            type(PUST_Planar_RectCub_type) :: this
+            type(PUST_RectCub_type) :: this
             real(C_DOUBLE) :: surface_tension_coefficient
+            real(IRL_double), dimension(1:3), intent(out) :: start_point
+            real(IRL_double), dimension(1:3), intent(out) :: end_point
             real(IRL_double), dimension(1:3) :: a_force
-            call F_PUST_Planar_RectCub_solve(this%c_object, surface_tension_coefficient, a_force)
+            call F_PUST_RectCub_solveEdge(this%c_object, surface_tension_coefficient,start_point,end_point, a_force)
             return
-        end subroutine PUST_Planar_RectCub_class_solve
+        end subroutine PUST_RectCub_class_solveEdge
 
-end module f_PUSolve_Planar_RectCub_class
+end module f_PUSolve_RectCub_class
