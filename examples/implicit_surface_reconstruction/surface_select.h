ad35712715c55b97231876cd583eadb4fefba09d
Parin Trivedi <parinht2@illinois.edu>
2025-11-16 22:41:48 -0600

    increasing bounds to handle coarse mesh + more refinements

--- examples/implicit_surface_reconstruction/surface_select.h
+++ examples/implicit_surface_reconstruction/surface_select.h
@@ -19,10 +19,10 @@
 #include "irl/geometry/implicit_surfaces/implicit_surfaces.h"
 
 // refine levels for initializing moments (for Nx = 256)
-constexpr std::size_t SPHERE_MAX_REFINE = 5;
-constexpr std::size_t ELLIPSOID_MAX_REFINE = 5;
+constexpr std::size_t SPHERE_MAX_REFINE = 6;
+constexpr std::size_t ELLIPSOID_MAX_REFINE = 6;
 constexpr std::size_t GENUS_MAX_REFINE = 5;
-constexpr std::size_t ORTHOCIRCLE_MAX_REFINE = 5;
+constexpr std::size_t ORTHOCIRCLE_MAX_REFINE = 6;
 
 using SphereVariant = IRL::Sphere<double, SPHERE_MAX_REFINE>;
 using EllipsoidVariant = IRL::Ellipsoid<double, ELLIPSOID_MAX_REFINE>;
@@ -34,18 +34,18 @@ using SurfaceVariant = std::variant<SphereVariant, EllipsoidVariant,
 
 inline SurfaceVariant makeSurface(const std::string& name, BasicMesh& mesh) {
   if (name == "sphere") {
-    mesh.setCellBoundaries(IRL::Pt(-0.18, -0.18, -0.18),
-                           IRL::Pt(0.18, 0.18, 0.18));
+    mesh.setCellBoundaries(IRL::Pt(-0.22, -0.22, -0.22),
+                           IRL::Pt(0.22, 0.22, 0.22));
     return SphereVariant(0., 0., 0., 0.15);
   } else if (name == "ellipsoid") {
-    mesh.setCellBoundaries(IRL::Pt(-0.32, -0.32, -0.32),
-                           IRL::Pt(0.32, 0.32, 0.32));
+    mesh.setCellBoundaries(IRL::Pt(-0.35, -0.35, -0.35),
+                           IRL::Pt(0.35, 0.35, 0.35));
     return EllipsoidVariant(0., 0., 0., 0.30, 0.15, 0.10);
   } else if (name == "genus") {
     mesh.setCellBoundaries(IRL::Pt(-2, -2, -2), IRL::Pt(2, 2, 2));
     return GenusVariant{};
   } else if (name == "orthocircle") {
-    mesh.setCellBoundaries(IRL::Pt(-1.5, -1.5, -1.5), IRL::Pt(1.5, 1.5, 1.5));
+    mesh.setCellBoundaries(IRL::Pt(-1.9, -1.9, -1.9), IRL::Pt(1.9, 1.9, 1.9));
     return OrthocircleVariant{};
   }
   throw std::runtime_error("Unknown surface: " + name +
