665d0e8c0936089b4e4e67af59627d25c7a18175
Fabien Evrard <fa.evrard@gmail.com>
2026-07-09 10:28:23 -0500

    Removing assert leading to compilation error in DEBUG mode

--- irl/c_interface/variant_reconstruction/c_separator_variant.cpp
+++ irl/c_interface/variant_reconstruction/c_separator_variant.cpp
@@ -96,8 +96,6 @@ void c_SeparatorVariant_getPlane(c_SeparatorVariant* a_self, const int* a_index,
   assert(a_self != nullptr);
   assert(a_self->obj_ptr != nullptr);
   assert(*a_index >= 0);
-  assert(static_cast<IRL::UnsignedIndex_t>(*a_index) <
-         a_self->obj_ptr->getNumberOfPlanes());
   if (IRL::PlanarSeparator* separator =
           std::get_if<IRL::PlanarSeparator>(a_self->obj_ptr)) {
     a_plane_listed[0] =
